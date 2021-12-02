ebcee3cc5ec00165a7fafd4750137f64569a8e1e
Brad <brad.cocar@gmail.com>
2021-12-02 14:43:10 -0800

    Project switching added

--- src/index.js
+++ src/index.js
@@ -1,41 +1,54 @@
 import './style.css';
 
+const projecthandler = (() => {
+    let projects = [];
+    let currentProjectID = 0;
+    const createProject = function(name, items) {
+        return {name, items};
+    }
+    function addProject(project) {
+        projects.push(project);
+    }
+    function switchProject(projectID) {
+        projecthandler.currentProjectID = projectID;
+        domhandler.updateProjectLibrary();
+        domhandler.updateNoteLibrary();
+    }
+    return {createProject, addProject, projects, currentProjectID, switchProject};
+})();
+const defaultProject = projecthandler.createProject('Default Project', []);
+projecthandler.addProject(defaultProject);
+const projectTwo = projecthandler.createProject('Project Two', []);
+projecthandler.addProject(projectTwo);
+
 const notehandler = (() => {
     const createNote = function(title, description, dueDate, priority, notes, checklist) {
-        defaultProject.items.push({title, description, dueDate, priority, notes, checklist});
+        projecthandler.projects[projecthandler.currentProjectID].items.push({title, description, dueDate, priority, notes, checklist});
     }
     function removeNote(noteID) {
-        defaultProject.items.splice(noteID, 1);
+        projecthandler.projects[projecthandler.currentProjectID].items.splice(noteID, 1);
         domhandler.updateNoteLibrary();
     }
     function updateDone(noteID) {
-        if(defaultProject.items[noteID].checklist === false) {
-            defaultProject.items[noteID].checklist = true;
-        } else if(defaultProject.items[noteID].checklist === true) {
-            defaultProject.items[noteID].checklist = false;
+        if(projecthandler.projects[projecthandler.currentProjectID].items[noteID].checklist === false) {
+            projecthandler.projects[projecthandler.currentProjectID].items[noteID].checklist = true;
+        } else if(projecthandler.projects[projecthandler.currentProjectID].items[noteID].checklist === true) {
+            projecthandler.projects[projecthandler.currentProjectID].items[noteID].checklist = false;
         }
         domhandler.updateNoteLibrary();
     }
     function updatePrio(noteID) {
-        if(defaultProject.items[noteID].priority === 'low') {
-            defaultProject.items[noteID].priority = 'medium';
-        } else if(defaultProject.items[noteID].priority === 'medium') {
-            defaultProject.items[noteID].priority = 'high';
-        } else if(defaultProject.items[noteID].priority === 'high') {
-            defaultProject.items[noteID].priority = 'low';
+        if(projecthandler.projects[projecthandler.currentProjectID].items[noteID].priority === 'low') {
+            projecthandler.projects[projecthandler.currentProjectID].items[noteID].priority = 'medium';
+        } else if(projecthandler.projects[projecthandler.currentProjectID].items[noteID].priority === 'medium') {
+            projecthandler.projects[projecthandler.currentProjectID].items[noteID].priority = 'high';
+        } else if(projecthandler.projects[projecthandler.currentProjectID].items[noteID].priority === 'high') {
+            projecthandler.projects[projecthandler.currentProjectID].items[noteID].priority = 'low';
         }
         domhandler.updateNoteLibrary();
     }
     return {createNote, removeNote, updateDone, updatePrio};
 })();
-
-const projecthandler = (() => {
-    const createProject = function(name, items) {
-        return {name, items};
-    }
-    return {createProject};
-})();
-const defaultProject = projecthandler.createProject('Default', []);
 
 const domhandler = (() => {
     const createNoteListener = (() => {
@@ -52,7 +65,7 @@ const domhandler = (() => {
     function updateNoteLibrary() {
         const noteHolder = document.querySelector('.notes');
         noteHolder.querySelectorAll('div').forEach(removeExisting => removeExisting.remove());
-        for(let i = 0; i < defaultProject.items.length; i++) {
+        for(let i = 0; i < projecthandler.projects[projecthandler.currentProjectID].items.length; i++) {
             const newNoteDOM = document.createElement('div');
             newNoteDOM.setAttribute('note-id', i);
             const headerContainer = document.createElement('div');
@@ -66,22 +79,22 @@ const domhandler = (() => {
             newNoteDOM.appendChild(optionsContainer);
             const noteTitle = document.createElement('h2');
             noteTitle.classList.add('note-title');
-            noteTitle.textContent = defaultProject.items[i].title;
+            noteTitle.textContent = projecthandler.projects[projecthandler.currentProjectID].items[i].title;
             headerContainer.appendChild(noteTitle);
             const noteDesc = document.createElement('p');
             noteDesc.classList.add('note-desc');
-            noteDesc.textContent = defaultProject.items[i].description;
+            noteDesc.textContent = projecthandler.projects[projecthandler.currentProjectID].items[i].description;
             bodyContainer.appendChild(noteDesc);
             const noteDue = document.createElement('p');
-            noteDue.textContent = `Due ${defaultProject.items[i].dueDate}`;
+            noteDue.textContent = `Due ${projecthandler.projects[projecthandler.currentProjectID].items[i].dueDate}`;
             bodyContainer.appendChild(noteDue);
             const priority = document.createElement('div');
             priority.classList.add('prio-circle');
-            if(defaultProject.items[i].priority === 'low') {
+            if(projecthandler.projects[projecthandler.currentProjectID].items[i].priority === 'low') {
                 priority.style.backgroundColor = 'green';
-            } else if(defaultProject.items[i].priority === 'medium') {
+            } else if(projecthandler.projects[projecthandler.currentProjectID].items[i].priority === 'medium') {
                 priority.style.backgroundColor = 'rgb(189, 189, 4)';
-            } else if(defaultProject.items[i].priority === 'high') {
+            } else if(projecthandler.projects[projecthandler.currentProjectID].items[i].priority === 'high') {
                 priority.style.backgroundColor = 'red';
             }
             headerContainer.appendChild(priority);
@@ -91,9 +104,9 @@ const domhandler = (() => {
             removeButton.style.visibility = 'hidden';
             const checklistButton = document.createElement('div');
             checklistButton.classList.add('check-note');
-            if(defaultProject.items[i].checklist === false){
+            if(projecthandler.projects[projecthandler.currentProjectID].items[i].checklist === false){
                 checklistButton.textContent = '';
-            } else if(defaultProject.items[i].checklist === true) {
+            } else if(projecthandler.projects[projecthandler.currentProjectID].items[i].checklist === true) {
                 checklistButton.textContent = '✔';
             }
             optionsContainer.appendChild(checklistButton);
@@ -132,5 +145,20 @@ const domhandler = (() => {
             notehandler.updatePrio(prio.closest('.note').getAttribute('note-id'))
         }));
     }
-    return {updateNoteLibrary};
+    function updateProjectLibrary() {
+        const projectTitle = document.querySelector('.manager-title');
+        projectTitle.textContent = projecthandler.projects[projecthandler.currentProjectID].name;
+        const projectContainer = document.querySelector('.projects-list');
+        projectContainer.querySelectorAll('div').forEach(remove => remove.remove());
+        for(let i = 0; i < projecthandler.projects.length; i++) {
+            const projectDIV = document.createElement('div');
+            projectDIV.classList.add('project');
+            projectDIV.textContent = projecthandler.projects[i].name;
+            projectDIV.setAttribute('project-id', i);
+            projectDIV.addEventListener('click', () => projecthandler.switchProject(projectDIV.getAttribute('project-id')));
+            projectContainer.appendChild(projectDIV);
+        }
+    }
+    updateProjectLibrary();
+    return {updateNoteLibrary, updateProjectLibrary};
 })();
